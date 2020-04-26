184faa83494b63464cdd1b3740dc476b5b444222
unknown <nhussaini@luc.edu>
2020-04-26 10:17:26 -0700

    complete the couter with redux

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -3,6 +3,29 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) =>{
+    if(action.type === 'INCREMENT'){
+        return {
+            counter: state.counter + 1
+        }
+    }
+
+    if(action.type === 'DECREMENT'){
+        return {
+            counter: state.counter - 1
+        }
+    }
+
+    if(action.type === 'ADD'){
+        return {
+            counter: state.counter + 10
+        }
+    }
+
+    if(action.type === 'SUBTRACT'){
+        return {
+            counter: state.counter - 8
+        }
+    }
     return state;
 
 };
